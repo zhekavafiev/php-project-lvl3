2ff7bc7405710b8b7d8d8257a1f248084ad36db5
Evg <zhekavafiev#gmail.com>
2020-06-02 12:35:25 +0500

    Tesr

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'MainPageController@index')->name('main.index');
 Route::post('/', 'MainPageController@store')->name('store');
-
 Route::get('/domains', 'DomainController@index')->name('domains.index');
-
 Route::get('/domains/{id}', 'DomainController@show')->name('domain');
-
 Route::post('/domains/{id}/check', 'DomainCheckController@check')->name('check');
+
+Route::get('/123', function () {
+    return view('test');
+});
