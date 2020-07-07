cbed43b3a3e7c5b7caec3b0b88fbf6a710d5d449
Evg <zhekavafiev#gmail.com>
2020-07-07 22:12:49 +0500

    Remove domain model from checks test

--- tests/Feature/DomainCheckTest.php
+++ tests/Feature/DomainCheckTest.php
@@ -3,20 +3,30 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Faker\Generator\Factory as Faker;
+use Faker\Factory;
 use App\Domain;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
 class DomainCheckTest extends TestCase
 {
     use DatabaseMigrations;
 
-    public function testAddChech()
+    public function testAddCheck()
     {
-        $domain = factory(Domain::class)->make();
-        $parsedName = parse_url($domain->name);
-        $domain->name = "{$parsedName['scheme']}://{$parsedName['host']}";
-        $domain->save();
+        // $domain = factory(Domain::class)->make();
+        $faker = Factory::create();
+        $domain = [
+            'name' => $faker->url,
+            'id' => 1
+        ];
+        $parsedName = parse_url($domain['name']);
+        $domain['name'] = "{$parsedName['scheme']}://{$parsedName['host']}";
+        $this->post(route('domains.store', $domain));
+        // dd(DB::select('select * from domains'));
+        // $domain->save();
         
         $statusCode = rand(300, 500);
         $h1 = 'h1';
@@ -24,7 +34,7 @@ class DomainCheckTest extends TestCase
         $description = 'description';
         
         Http::fake([
-            $domain->name => Http::response(
+            $domain['name'] => Http::response(
                 "<h1>{$h1}</h1>" .
                 "<meta name=\"keywords\" content=\"{$keywords}\">" .
                 "<meta name=\"description\" content=\"{$description}\">",
@@ -32,11 +42,11 @@ class DomainCheckTest extends TestCase
             )
         ]);
 
-        $response = $this->post(route('check', ['id' => $domain->id]));
+        $response = $this->post(route('check', ['id' => $domain['id']]));
         $response->assertStatus(302);
         $response->assertSessionHasNoErrors();
         $this->assertDatabaseHas('domain_checks', [
-            'domain_id' => $domain->id,
+            'domain_id' => $domain['id'],
             'status_code' => $statusCode,
             'keywords' => $keywords,
             'h1' => $h1,
