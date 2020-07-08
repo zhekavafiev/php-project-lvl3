ebcc75b978cb38b1f803cab1ee1b46d50485c782
Evg <zhekavafiev#gmail.com>
2020-07-08 15:22:23 +0500

    Rewrite tests with setUp method

--- tests/Feature/DomainControllerTest.php
+++ tests/Feature/DomainControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Domain;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Support\Arr;
@@ -14,26 +15,43 @@ class DomainControllerTest extends TestCase
 {
     use DatabaseMigrations;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+        for ($i = 0; $i < 3; $i++) {
+            $name = Factory::create()->url;
+            $parsedName = parse_url($name);
+            $name = "{$parsedName['scheme']}://{$parsedName['host']}";
+            DB::insert('insert into domains (name) values (?)', [$name]);
+        }
+    }
+
+    public function testDomainStore()
+    {
+        $faker = Factory::create();
+        $data['name'] = $faker->url;
+        $parsedName = parse_url($data['name']);
+        $data['name'] = "{$parsedName['scheme']}://{$parsedName['host']}";
+        $response = $this->post(route('domains.store'), $data);
+        $response->assertSessionHasNoErrors();
+        $response->assertStatus(302);
+        $this->assertDatabaseHas('domains', $data);
+    }
+
     public function testDomainShow()
     {
-        $domain['name'] = Factory::create()->url;
-        $domain['id'] = 1;
-        $parsedName = parse_url($domain['name']);
-        $domain['name'] = "{$parsedName['scheme']}://{$parsedName['host']}";
-        $this->post(route('domains.store', $domain));
+        $domain = DB::table('domains')
+            ->select('*')
+            ->limit(1)
+            ->get()->toArray()[0];
 
-        $response = $this->get(route('domains.show', $domain['id']));
+        $response = $this->get(route('domains.show', $domain->id));
         $response->assertStatus(200);
         $response->assertSessionHasNoErrors();
     }
 
     public function testIndexWrongPaginatePage()
     {
-        $domain['name'] = Factory::create()->url;
-        $parsedName = parse_url($domain['name']);
-        $domain['name'] = "{$parsedName['scheme']}://{$parsedName['host']}";
-        $this->post(route('domains.store', $domain));
-        
         $wrongPage = rand(2, 100);
         $response = $this->get(route('domains.index', ['page' => $wrongPage]));
         $response->assertStatus(302);
@@ -42,7 +60,8 @@ class DomainControllerTest extends TestCase
 
     public function testDomainPageNotHasOnDB()
     {
-        $id = rand(0, 100);
+        $count = DB::table('domains')->count();
+        $id = rand($count + 1, 100);
         $response = $this->get(route('domains.show', ['id' => $id]));
         $response->assertStatus(404);
     }
